c8af75e3c6076f9df5447da362ca0020de61c6f5
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-03 19:18:45 +0100

    adding css and js files

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+<div class="tab-pane active" id="hn">
+  <div class="list-group">
+    <a href="http://www.computerworld.com/s/article/print/9248070/SanDisk_announces_4TB_SSD_hopes_for_8TB_next_year" class="list-group-item active">
+      <h4 class="list-group-item-heading">SanDisk announces 4TB SSD, hopes for 8TB next year.</h4>
+      <p class="list-group-item-text"> (computerworld.com) </p>
+    </a>
+    <a href="http://www.reuters.com/article/2014/05/03/us-microsoft-gates-idUSBREA410YS20140503" class="list-group-item">
+        <h4 class="list-group-item-heading">Bill Gates on track to own no Microsoft stock in four years</h4>
+        <p class="list-group-item-text"> (reuters.com) </p>
+    </a>
+    <a href="http://www.slate.com/articles/technology/future_tense/2014/05/fcc_chairman_tom_wheeler_s_lame_excuses_for_his_net_neutrality_proposal.html" class="list-group-item">
+      <h4 class="list-group-item-heading">FCC Chairman Tom Wheeler’s lame excuses for his net neutrality proposal.</h4>
+      <p class="list-group-item-text"> (slate.com) </p>
+    </a>
+  </div>
+</div>
+
+
+
+function renderNewsItem( item ) {
+'<a href="' + url + '" class="list-group-item active">'
+'  <h4 class="list-group-item-heading">' + title + '</h4>'
+'  <p class="list-group-item-text">' + src + '</p>'
+'</a>'
+}
+
